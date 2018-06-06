ab4b49341d1690ea7adb00febba4b42efe323614
Cindy <cindynhoang@gmail.com>
2018-06-06 16:20:25 -0700

    added components for arc

--- src/components/Structures/ARC/Arc.js
+++ src/components/Structures/ARC/Arc.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
 import ArcMap from './Map';
 
 export default class Arc extends Component {
 	render() {
 		return(
 			<View style={styles.container}>
+				<View style={styles.logoContainer}>
+					<Text style={styles.logo}>ParkDavis</Text>
+				</View>
+				<View style={styles.titleContainer}>
+					<Text style={styles.title}>ARC</Text>
+				</View>
+				<View style={styles.contentContainer}>
+					<ArcMap />
+				</View>
+				<View style={styles.selectionContainer}>
+					<Text style={styles.selectionTitle}>Please select your spot</Text>
+					<Text style={styles.selectionSubtitle}>Nearby spots available:</Text>
+
+					<View style={styles.spotContainer}>
+						<TouchableOpacity style={styles.buttonContainer}>
+							<Text style={styles.buttonText}>Spot 1</Text>
+						</TouchableOpacity>
+
+						<TouchableOpacity style={styles.buttonContainer}>
+							<Text style={styles.buttonText}>Spot 2</Text>
+						</TouchableOpacity>
+
+						<TouchableOpacity style={styles.buttonContainer}>
+							<Text style={styles.buttonText}>Spot 3</Text>
+						</TouchableOpacity>
+					</View>
+				</View>
 			</View>
 		);
 	}
@@ -13,6 +40,62 @@ export default class Arc extends Component {
 
 const styles = StyleSheet.create({
 	container: {
-
+		flex: 1,
+		justifyContent: 'center'
+	},
+	logoContainer: {
+		backgroundColor: '#EB9532',
+		padding: 30
+	},
+	titleContainer: {
+		backgroundColor: 'white',
+		padding: 20,
+	},
+	contentContainer: {
+		backgroundColor: 'rgba(235,149,50,0.2)',
+		flexGrow: 1
+	},
+	selectionContainer: {
+		backgroundColor: '#EB9532',
+		height: 160,
+		padding: 20,
+	},
+	spotContainer: {
+		flexDirection: 'row'
+	},
+	buttonContainer: {
+		backgroundColor: 'white',
+		width: 100,
+		paddingVertical: 15,
+		marginRight: 20,
+		borderRadius: 20
+	},
+	logo: {
+		color: 'white',
+		textAlign: 'center',
+		fontSize: 20,
+		fontWeight: 'bold'
+	},
+	title: {
+		color: '#EB9532',
+		textAlign: 'center',
+		fontSize: 20,
+		fontWeight: 'bold'
+	},
+	selectionTitle: {
+		color: 'white',
+		fontSize: 20,
+		marginBottom: 10,
+		textAlign: 'center',
+		fontWeight: 'bold'
+	},
+	selectionSubtitle: {
+		color: 'white',
+		fontSize: 15,
+		marginBottom: 10
+	},
+	buttonText: {
+		textAlign: 'center',
+		color: '#EB9532'
 	}
 });
